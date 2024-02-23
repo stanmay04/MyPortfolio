5dfd2e92c08adb0907030995debcd2e796116732
ragnarok04 <tanmay8265@gmail.com>
2024-02-23 22:21:59 +0530

    secpnd update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('5s', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [
+        animate('5s', style({ opacity: 0 })),
+      ]),
+    ]),
+  ],
 })
 export class AppComponent {
   title = 'myportfolio';
+  isLoaded: boolean = false;
+
+  // Simulate a delay to show the animation
+  ngOnInit() {
+    setTimeout(() => {
+      this.isLoaded = true;
+    }, 5000); // Adjust the delay as needed
+  }
 }
